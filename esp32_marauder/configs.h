102faa17a4b7413724d9641f9d9ab6b25185205c
igniskarya <karyaignis@gmail.com>
2026-03-13 03:24:15 +0700

    Update configs.h

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -1335,7 +1335,7 @@
 
       #define SCREEN_CHAR_WIDTH 40
       #define HAS_ILI9341
-      #define HAS_ST7789
+      //#define HAS_ST7789
     
       #define BANNER_TEXT_SIZE 2
 
@@ -1401,44 +1401,161 @@
     #endif
 
     #if defined(MARAUDER_CYD_3_5_INCH)
-      #define CHAN_PER_PAGE 7
+       #define SCREEN_CHAR_WIDTH 40
+        #define HAS_ST7796
+        #define BANNER_TEXT_SIZE 2
 
-      #define SCREEN_CHAR_WIDTH 40
-      //#define HAS_ILI9341
-      #define HAS_ST7796
+        #define TFT_BL   27            // LED back-light control pin
+        #define TFT_BACKLIGHT_ON HIGH  // Level to turn ON back-light (HIGH or LOW)
+
+        #define TFT_MISO 12 // or SDO
+        #define TFT_MOSI 13 // In some display driver board, it might be written as "SDA" or SDI
+        #define TFT_SCLK 14
+        #define TFT_CS   15  // Chip select control pin
+        #define TFT_DC   2  // Data Command control pin (OR RS)
+        #define TFT_RST  -1  // Reset pin 
+        #define TFT_BL   27  // LED back-light
+        #define THROW_AWAY_TOUCH_COUNT 31
+        
+        #define SPI_FREQUENCY  65000000
+        #define SPI_READ_FREQUENCY  20000000
+        #define SPI_TOUCH_FREQUENCY  2500000  //2500000
+
+        // #define TOUCH_CS 33     // Chip select pin (T_CS) of touch screen
+        
+        #define TOUCH_SDA  33
+        #define TOUCH_SCL  32
+        #define TOUCH_INT 21
+        #define TOUCH_RST 25
+
+        #define TOUCH_WIDTH  320
+        #define TOUCH_HEIGHT 480
+
+        // Change the width and height if required (defined in portrait mode)
+        // or use the constructor to over-ride defaults
     
-      #define BANNER_TEXT_SIZE 2
-
-      #ifndef TFT_WIDTH
-        #define TFT_WIDTH 320
-      #endif
-
-      #ifndef TFT_HEIGHT
+        #define TFT_WIDTH  320
         #define TFT_HEIGHT 480
-      #endif
 
-      #define TFT_DIY
+        #define TFT_NOP     0x00
+        #define TFT_SWRST   0x01
 
-      #define GRAPH_VERT_LIM TFT_HEIGHT/2 - 1
+        #define TFT_INVOFF  0x20
+        #define TFT_INVON   0x21
 
-      #define EXT_BUTTON_WIDTH 30
+        #define TFT_DISPOFF 0x28
+        #define TFT_DISPON  0x29
 
-      #define SCREEN_BUFFER
+        #define TFT_CASET   0x2A
+        #define TFT_PASET   0x2B
+        #define TFT_RAMWR   0x2C
+        #define TFT_RAMRD   0x2E
 
-      #define MAX_SCREEN_BUFFER 33
+        #define TFT_MADCTL  0x36
+        #define TFT_MAD_MY  0x80
+        #define TFT_MAD_MX  0x40
+        #define TFT_MAD_MV  0x20
+        #define TFT_MAD_ML  0x10
+        #define TFT_MAD_BGR 0x08
+        #define TFT_MAD_MH  0x10
+        #define TFT_MAD_RGB 0x00
 
-      #define SCREEN_ORIENTATION 0
-    
+        #define SCREEN_BUFFER
+        #define MAX_SCREEN_BUFFER 33
+
+        #ifdef TFT_RGB_ORDER
+          #if (TFT_RGB_ORDER == 1)
+            #define TFT_MAD_COLOR_ORDER TFT_MAD_RGB
+          #else
+            #define TFT_MAD_COLOR_ORDER TFT_MAD_BGR
+          #endif
+        #else
+          #define TFT_MAD_COLOR_ORDER TFT_MAD_BGR
+        #endif
+
+        // ST7796 specific commands
+        #define ST7796_NOP     0x00
+        #define ST7796_SWRESET 0x01
+        #define ST7796_RDDID   0x04
+        #define ST7796_RDDST   0x09
+
+        #define ST7796_SLPIN   0x10
+        #define ST7796_SLPOUT  0x11
+        #define ST7796_PTLON   0x12
+        #define ST7796_NORON   0x13
+
+        #define ST7796_RDMODE  0x0A
+        #define ST7796_RDMADCTL  0x0B
+        #define ST7796_RDPIXFMT  0x0C
+        #define ST7796_RDIMGFMT  0x0A
+        #define ST7796_RDSELFDIAG  0x0F
+
+        #define ST7796_INVOFF  0x20
+        #define ST7796_INVON   0x21
+
+        #define ST7796_DISPOFF 0x28
+        #define ST7796_DISPON  0x29
+
+        #define ST7796_CASET   0x2A
+        #define ST7796_PASET   0x2B
+        #define ST7796_RAMWR   0x2C
+        #define ST7796_RAMRD   0x2E
+
+        #define ST7796_PTLAR   0x30
+        #define ST7796_VSCRDEF 0x33
+        #define ST7796_MADCTL  0x36
+        #define ST7796_VSCRSADD 0x37
+        #define ST7796_PIXFMT  0x3A
+
+        #define ST7796_WRDISBV  0x51
+        #define ST7796_RDDISBV  0x52
+        #define ST7796_WRCTRLD  0x53
+
+        #define ST7796_FRMCTR1 0xB1
+        #define ST7796_FRMCTR2 0xB2
+        #define ST7796_FRMCTR3 0xB3
+        #define ST7796_INVCTR  0xB4
+        #define ST7796_DFUNCTR 0xB6
+
+        #define ST7796_PWCTR1  0xC0
+        #define ST7796_PWCTR2  0xC1
+        #define ST7796_PWCTR3  0xC2
+
+        #define ST7796_VMCTR1  0xC5
+        #define ST7796_VMCOFF  0xC6
+
+        #define ST7796_RDID4   0xD3
+
+        #define ST7796_GMCTRP1 0xE0
+        #define ST7796_GMCTRN1 0xE1
+
+        #define ST7796_MADCTL_MY  0x80
+        #define ST7796_MADCTL_MX  0x40
+        #define ST7796_MADCTL_MV  0x20
+        #define ST7796_MADCTL_ML  0x10
+        #define ST7796_MADCTL_RGB 0x00
+        #define ST7796_MADCTL_BGR 0x08
+        #define ST7796_MADCTL_MH  0x04
+
+        #define GRAPH_VERT_LIM TFT_HEIGHT/2
+
+      #define EXT_BUTTON_WIDTH 20
+
+    //#if defined(CYD_35) || defined(CYD_35CAP)
+      //#define TEXT_HEIGHT 24 // Height of text to be printed and scrolled
+    //#else
+      #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+    //#endif
       #define CHAR_WIDTH 12
       #define SCREEN_WIDTH TFT_WIDTH
       #define SCREEN_HEIGHT TFT_HEIGHT
       #define HEIGHT_1 TFT_WIDTH
       #define WIDTH_1 TFT_HEIGHT
-      #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6) // number of characters on a single line with normal font
-      #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+      #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH / 6) // number of characters on a single line with normal font
+      
       #define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
       #define TOP_FIXED_AREA 48 // Number of lines in top fixed area (lines counted from top of screen)
-      #define YMAX 480 // Bottom of screen area
+      #define YMAX TFT_HEIGHT // Dynamically set based on the display height
       #define minimum(a,b)     (((a) < (b)) ? (a) : (b))
       //#define MENU_FONT NULL
       #define MENU_FONT &FreeMono9pt7b // Winner
@@ -1452,24 +1569,25 @@
 
       #define FRAME_X 100
       #define FRAME_Y 64
-      #define FRAME_W TFT_WIDTH / 2
+      #define FRAME_W 120
       #define FRAME_H 50
-    
+
       // Red zone size
       #define REDBUTTON_X FRAME_X
       #define REDBUTTON_Y FRAME_Y
-      #define REDBUTTON_W (FRAME_W/2)
+      #define REDBUTTON_W (FRAME_W / 2)
       #define REDBUTTON_H FRAME_H
-    
+
       // Green zone size
       #define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
       #define GREENBUTTON_Y FRAME_Y
-      #define GREENBUTTON_W (FRAME_W/2)
+      #define GREENBUTTON_W (FRAME_W / 2)
       #define GREENBUTTON_H FRAME_H
-    
-      #define STATUSBAR_COLOR 0x4A49
-    
-      #define KIT_LED_BUILTIN 13
+
+      #define STATUSBAR_COLOR 0x18ED
+
+      #define KIT_LED_BUILTIN 4
+
     #endif
 
     #if defined(MARAUDER_CYD_GUITION)
